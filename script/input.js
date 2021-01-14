1df880a433ccf9df24d0229e976e3b0c6a403b60
azalea123 <tsutsuji1126@gmail.com>
2021-01-14 19:51:14 +0900

    打ったら色が変わるようになりました

--- script/input.js
+++ script/input.js
@@ -3,7 +3,8 @@ var succsess_count = 0
 var sum_succsess_count = 0
 var miss_count = 0
 var sum_miss_count = 0
-var words = ["helloworld","goodnight"]
+var word_num = 0
+var words = ["helloworld", "goodnight", "yonedakazuki"]
 
 window.onload = function (){
     document.getElementById("typing_word").innerHTML = words[0];
@@ -23,30 +24,40 @@ document.onkeydown = function(e) {
             key = Event.which;
         }
     }
-    /*　配列arry_textに１文字ずつ文字を代入　*/
-    /*　i < typing_word.lengthで文字数分だけ繰り返す　*/
-    for(let i = 0; i < typing_word.length; i++) {
-        array_text[i] = typing_word[i]
+
+
+    if (succsess_count == 0 && miss_count == 0) {
+        /*　配列arry_textに１文字ずつ文字を代入　*/
+        /*　i < typing_word.lengthで文字数分だけ繰り返す　*/
+        for(let i = 0; i < words[word_num].length; i++) {
+        array_text[i] = words[word_num][i]
+        }
     }
+
     judge(array_text[succsess_count],key)
     console.log("sum_succsess_count="+sum_succsess_count)
     console.log("sum_miss_count="+sum_miss_count)
 };
+
 function judge(one_letter,key){
     if(key === one_letter) {
-        array_text[succsess_count] = key.toUpperCase();
+        array_text[succsess_count] = "<font color='red'>" + array_text[succsess_count] + "</font>";
+
         succsess_count++
-        sum_succsess_count++      
+        sum_succsess_count++
+
         var string = array_text.join("");       
         document.getElementById("typing_word").innerHTML = string
     }else {
         miss_count++
         sum_miss_count++
     }
-    if(succsess_count === words[0].length) {
-        document.getElementById("typing_word").innerHTML = words[1];
+    if(succsess_count === words[word_num].length) {
+        document.getElementById("typing_word").innerHTML = words[word_num + 1];
+
         succsess_count = 0
         miss_count = 0
         array_text = [] 
+        word_num += 1
     }
 }
